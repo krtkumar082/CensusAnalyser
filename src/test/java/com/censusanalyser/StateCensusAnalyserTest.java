1a92f5a1593ce3449ccf3dc7c1c6a666982666ae
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-25 23:57:23 +0530

    [Kirti]Added:tested to ensure the number of records matches

---
+++ src/test/java/com/censusanalyser/StateCensusAnalyserTest.java
@@ -0,0 +1,31 @@
+package com.censusanalyser;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.censusanalyser.CensusAnalyserException;
+import com.censusanalyser.StateCensusAnalyser;
+
+public class StateCensusAnalyserTest {
+	private StateCensusAnalyser stateCensusAnalyser;
+	public static final String STATE_CENSUS_FILE_PATH = "./IndiaStateCensusData.csv";
+	private static final String STATE_CENSUS_WRONG_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+	private static final String CENSUS_WRONG_TYPE_FILE_PATH = "./IndiaStateCensusData.txt";
+	private static final String CENSUS_WRONGHEADER_FILE_PATH = "./IndiaStateCode.csv";
+	private static final String CENSUS_WRONG_DELIMITER_FILE_PATH = "./CensusDelimiter.csv";
+
+	@Before
+	public void initialize() {
+		stateCensusAnalyser = new StateCensusAnalyser();
+
+	}
+
+	@Test
+	public void givenStateCensusCSVFile_ShouldReturnNumberOfRecords() throws CensusAnalyserException {
+		int numOfEntries = stateCensusAnalyser.loadIndiaCensusData(STATE_CENSUS_FILE_PATH);
+		Assert.assertEquals(29, numOfEntries);
+	}
+
+	
+}
