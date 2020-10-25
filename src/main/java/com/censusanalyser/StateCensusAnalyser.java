6c0822269bc6f64afeecdb25878b0fd42052e5b8
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-10-26 00:18:13 +0530

    [Kirti]Added:loaded state code data

--- src/main/java/com/censusanalyser/StateCensusAnalyser.java
+++ src/main/java/com/censusanalyser/StateCensusAnalyser.java
@@ -36,5 +36,30 @@ public class StateCensusAnalyser {
 
 		}
 	}
+    
+	public int loadIndiaStateCodeData(String csvFilePath) throws CensusAnalyserException {
+		if (!csvFilePath.contains(".csv")) {
+			throw new CensusAnalyserException("Not CSV file",
+					CensusAnalyserException.ExceptionType.INCORRECT_FILE_TYPE);
+		}
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+			CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(CSVStateCensus.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
+			Iterator<CSVStateCensus> censusCSVIterator = csvToBean.iterator();
+			Iterable<CSVStateCensus> csvIterable = () -> censusCSVIterator;
+			int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+			return numOfEntries;
+		} catch (IOException e) {
+			throw new CensusAnalyserException("File not found",
+					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+		} catch (RuntimeException e) {
+			throw new CensusAnalyserException("File data not proper",
+					CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+
+		}
 	}
+	
+}
 
